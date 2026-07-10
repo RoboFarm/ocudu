c11fa79743aa28e59cdb6c9b4b082d48c9c9d425
Franciszek Maroszek <frank@srs.io>
2026-07-10 19:18:21 +0200

    sched: size rate estimator DMRS tables by layers instead of ports
    Signed-off-by: Franciszek Maroszek <frank@srs.io>

--- lib/scheduler/policy/scheduler_time_qos.cpp
+++ lib/scheduler/policy/scheduler_time_qos.cpp
@@ -93,25 +93,27 @@ rate_estimator::rate_estimator(const cell_configuration& cell_cfg) :
                  .tb_scaling_field = 1,
                  .n_prb            = cell_cfg.params.ul_cfg_common.init_ul_bwp.generic_params.crbs.length()}
 {
-  dl_dmrs_rbs_per_nof_layers.resize(cell_cfg.params.dl_carrier.nof_ant);
-  for (unsigned nof_layers = 1, max_layers = dl_dmrs_rbs_per_nof_layers.size(); nof_layers <= max_layers;
-       ++nof_layers) {
+  const unsigned nof_dl_ports  = cell_cfg.params.dl_carrier.nof_ant;
+  const unsigned max_dl_layers = cell_cfg.params.init_bwp.pdsch.max_nof_layers;
+  dl_dmrs_rbs_per_nof_layers.resize(max_dl_layers);
+  for (unsigned nof_layers = 1; nof_layers <= max_dl_layers; ++nof_layers) {
     const pdsch_time_domain_resource_allocation pdsch_td_cfg{
         0, sch_mapping_type::typeA, {0, NOF_OFDM_SYM_PER_SLOT_NORMAL_CP}};
     const dmrs_downlink_config dmrs_cfg{};
     auto                       dmrs = make_dmrs_info_dedicated(
-        pdsch_td_cfg, cell_cfg.params.pci, cell_cfg.params.dmrs_typeA_pos, dmrs_cfg, nof_layers, max_layers, false);
+        pdsch_td_cfg, cell_cfg.params.pci, cell_cfg.params.dmrs_typeA_pos, dmrs_cfg, nof_layers, nof_dl_ports, false);
     dl_dmrs_rbs_per_nof_layers[nof_layers - 1] = calculate_nof_dmrs_per_rb(dmrs);
   }
 
-  ul_dmrs_rbs_per_nof_layers.resize(cell_cfg.params.ul_carrier.nof_ant);
-  for (unsigned nof_layers = 1, max_layers = ul_dmrs_rbs_per_nof_layers.size(); nof_layers <= max_layers;
-       ++nof_layers) {
+  const unsigned nof_ul_ports  = cell_cfg.params.ul_carrier.nof_ant;
+  const unsigned max_ul_layers = cell_cfg.params.init_bwp.pusch.max_nof_layers;
+  ul_dmrs_rbs_per_nof_layers.resize(max_ul_layers);
+  for (unsigned nof_layers = 1; nof_layers <= max_ul_layers; ++nof_layers) {
     const pusch_time_domain_resource_allocation pusch_td_cfg{
         2, sch_mapping_type::typeA, {0, NOF_OFDM_SYM_PER_SLOT_NORMAL_CP}};
     const dmrs_uplink_config dmrs_cfg{};
     auto                     dmrs = make_dmrs_info_dedicated(
-        pusch_td_cfg, cell_cfg.params.pci, cell_cfg.params.dmrs_typeA_pos, dmrs_cfg, nof_layers, max_layers, false);
+        pusch_td_cfg, cell_cfg.params.pci, cell_cfg.params.dmrs_typeA_pos, dmrs_cfg, nof_layers, nof_ul_ports, false);
     ul_dmrs_rbs_per_nof_layers[nof_layers - 1] = calculate_nof_dmrs_per_rb(dmrs);
   }
 }
